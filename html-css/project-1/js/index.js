58b3be2a332a6745db6e42bb8fe13b4f61add7f4
kja-axelor <krjani.axelor@gmail.com>
2021-10-29 13:38:26 +0530

    Calculate age from given dob

--- html-css/project-1/js/index.js
+++ html-css/project-1/js/index.js
@@ -55,3 +55,43 @@ function cityvalidate(){
          console.log("city ok");
     }
 }
+function age(){
+    var dob = new Date(document.getElementById("dob").value);
+    var today = new Date();
+    if (dob.getTime() > today.getTime())
+    {
+        document.getElementById("age").innerHTML = "please provide proper birth date";
+        document.getElementById("age").style.color = "red";
+    }
+    else{       
+        const b_day = dob.getDate();
+        const b_month = dob.getMonth();
+        const b_year = dob.getFullYear();
+
+        const t_date = today.getDate();
+        const t_month = today.getMonth();
+        const t_year = today.getFullYear();
+
+        let age = 0;
+        if (t_month > b_month)
+        {
+            age = t_year - b_year;
+        }
+        else if(t_month == b_month){
+            if(t_date >= b_day){
+                age = t_year - b_year;
+            }
+            else{
+                age = t_year - b_year - 1;
+            }
+        }
+        else{
+            age = t_year - b_year - 1;
+        }
+        document.getElementById("age").innerHTML = `Age: ${age} years`;
+        document.getElementById("age").style.color = "green";
+        document.getElementById("age").style.fontWeight = "bold";
+    }
+    
+}
+
