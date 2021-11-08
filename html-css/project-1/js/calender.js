609398142562937dabb4ab7cdfbc35175245bdaa
kja-axelor <krjani.axelor@gmail.com>
2021-11-08 11:19:26 +0530

    Highlight fridays and sundays in calendar and modify css

--- html-css/project-1/js/calender.js
+++ html-css/project-1/js/calender.js
@@ -15,11 +15,7 @@ var calender = function () {
  .getDay();
 
   // LastDay(Previous month)
-  const prevLastDay = new Date(
-    date.getFullYear(),
-    date.getMonth(),
-    0
-  ).getDate();
+  const prevLastDay = new Date(date.getFullYear(),date.getMonth(),0).getDate();
 
   var months = [
     "January",
@@ -52,42 +48,62 @@ var calender = function () {
 
   document.querySelector(".months p").innerHTML = date.toDateString();
 
-  monthDays = document.querySelector("#days");
+ let  monthDays = document.querySelector("#days");
 
   // space logic (prev days)
   let days = ``;
+
   for (let j = firstdayIndex; j > 0; j--) {
-    days += `<td>${prevLastDay - j + 1}</td>`;
+    days += `<td class="disable">${prevLastDay - j + 1}</td>`;
     // days += `<td></td>`
   }
 
-  let count = firstdayIndex - 1;
   // Loop logic
+  let count = firstdayIndex - 1;
   for (let i = 1; i <= lastDay; i++) {
       count += 1;
       // row seperate
       if(count % 7 == 0){
           days += `</tr>`;
-          // count = firstdayIndex - 1;
+          count = 0;
       }
+
+      // display date
+
+      // today
+      if(i === new Date().getDate())
+      {
+          days += `<td class="today">${i}</td>`;
+      }
+      else if(count % 7 == 5){
+        // friday
+          days += `<td class="friday">${i}</td>`
+      }
+      else if(count % 7 == 0){
+          // Sunday
+         days += `<td class="sunday">${i}</td>`;
+      }
+      else{
           days += `<td>${i}</td>`;
+      }
   }
+
   // space logic (next days)
   const nextDays = 7 - lastDayIndex - 1;
   for (let index = 1; index <= nextDays; index++) {
-    days += `<td>${index}</td>`;
+      days += `<td class="disable">${index}</td>`;
     // days += `<td></td>`
   }
-
   days += `</tr>`;
   monthDays.innerHTML = days;
 };
 
-let upper = document.querySelector(".prev").addEventListener("click", () => {
+let upper = document.querySelector(".up").addEventListener("click", () => {
   date.setMonth(date.getMonth() + 1);
   calender();
 });
-let down = document.querySelector(".next").addEventListener("click", ()=>{
+
+let down = document.querySelector(".down").addEventListener("click", ()=>{
     date.setMonth(date.getMonth() - 1);
     calender();
 });
